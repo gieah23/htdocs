c0501c0619dbf9544971465d3cc9f91bbcc8d5b0
ambarwati20 <ambar8636@gmail.com>
2019-05-06 12:08:17 +0700

    p

---
+++ projek/application/controllers/admin/login.php
@@ -0,0 +1,20 @@
+<?php
+class auth extends CI_Controller{
+
+function __construct(){
+  parent::__construct();
+  $this->load->model('model_userLog');
+}
+
+  function login()
+  {
+    if(isset($_POST['submit'])){
+      $username = $this->input->post('username');
+      $password = $this->input->post('password');
+      $berhasil = $this->model_userLog->login($username,$password);
+      echo $berhasil;
+    }else{
+        $this->load->view('form_login');
+    }
+  }
+}
