8b0be221313379182e4d71e8558974235b5ef917
Waafiq29 <45926327+Waafiq29@users.noreply.github.com>
2019-05-20 06:51:07 -0700

    Update dasboard.php

--- Project_akhir/application/views/dasboard.php
+++ Project_akhir/application/views/dasboard.php
@@ -80,8 +80,8 @@
         </a>
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
           <h6 class="dropdown-header">Data:</h6>
-          <a class="dropdown-item" href="login.html">Siswa</a>
-          <a class="dropdown-item" href="register.html">Guru</a>
+          <a class="dropdown-item" href="datasiswa.html">Siswa</a>
+          <a class="dropdown-item" href="dataguru.html">Guru</a>
         </div>
       </li>
       <li class="nav-item dropdown">
@@ -91,10 +91,21 @@
         </a>
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
           <h6 class="dropdown-header">Presensi:</h6>
-          <a class="dropdown-item" href="login.html">Siswa</a>
-          <a class="dropdown-item" href="register.html">Guru</a>
+          <a class="dropdown-item" href="presensisiswa.html">Siswa</a>
+          <a class="dropdown-item" href="presensiguru.html">Guru</a>
         </div>
       </li>
+       <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Iput Data</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">Tables:</h6>
+          <a class="dropdown-item" href="inputdatasiswa.html">Input Data Siswa</a>
+          <a class="dropdown-item" href="inputdataguru.html">Input Data Guru</a>
+        </div>
+     </li>
     </ul>
 
   </nav>
