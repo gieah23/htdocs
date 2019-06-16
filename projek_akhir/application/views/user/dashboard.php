e2cf6066cb38cbc7e26c68e38543687557fe71be
Waafiq29 <45926327+Waafiq29@users.noreply.github.com>
2019-06-16 13:01:44 +0700

    Add files via upload

--- projek_akhir/application/views/user/dashboard.php
+++ projek_akhir/application/views/user/dashboard.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,62 +10,51 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin - Dashboard</title>
+  <title>SB Admin - Data Siswa</title>
 
   <!-- Custom fonts for this template-->
   <link href=<?php echo base_url("assets/fontawesome-free/css/all.min.css"); ?> rel="stylesheet" type="text/css">
 
-  <!-- Page level plugin CSS-->
-  <link href=<?php echo base_url("assets/datatables/dataTables.bootstrap4.css");?> rel="stylesheet">
+<!-- Page level plugin CSS-->
+<link href=<?php echo base_url("assets/datatables/dataTables.bootstrap4.css");?> rel="stylesheet">
 
-  <!-- Custom styles for this template-->
-  <link href=<?php echo base_url("css/sb-admin.css"); ?> rel="stylesheet">
+<!-- Custom styles for this template-->
+<link href=<?php echo base_url("css/sb-admin-2.css"); ?> rel="stylesheet">
 
 </head>
 
 <body id="page-top">
 
-  <?php include 'navbar.php' ?>
+    <?php include 'navbar.php' ?>
 
   <div id="wrapper">
 
     <?php include 'sidebar.php'?> 
 
-    <div id="content-wrapper">
+      <div id="content-wrapper">
 
       <div class="container-fluid">
-
-        <!-- Breadcrumbs-->
+      	<!-- Breadcrumbs-->
         <ol class="breadcrumb">
           <li class="breadcrumb-item">
             <a href="#">Dashboard</a>
           </li>
-					<li class="breadcrumb-item">
-            <a href="#">Dashboard</a>
-          </li>
           <li class="breadcrumb-item active">Overview</li>
         </ol>
-				<li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-fw fa-folder"></i>
-          <span>Data</span>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-          <h6 class="dropdown-header">Data:</h6>
-          <a class="dropdown-item" href="#">Siswa</a>
-          <a class="dropdown-item" href="#">Guru</a>
-					<a class="dropdown-item" href="#">Mapel</a>
-					<a class="dropdown-item" href="#">Jadwal</a>
-					<a class="dropdown-item" href="#">Kelas</a>
-        </div>
-      </li>
         
         <?php include 'datatable.php' ?>
 
       </div>
       <!-- /.container-fluid -->
 
-      <?php include 'footer.php' ?>
+      <!-- Sticky Footer -->
+      <footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright © Your Website 2019</span>
+          </div>
+        </div>
+      </footer>
 
     </div>
     <!-- /.content-wrapper -->
@@ -97,23 +87,23 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src=<?php echo base_url("assets/jquery/jquery.min.js"); ?>></script>
-  <script src=<?php echo base_url("assets/bootstrap/js/bootstrap.bundle.min.js"); ?>></script>
+  <script src=<?php echo base_url("assets/jquery/jquery.min.js");?>></script>
+  <script src=<?php echo base_url("assets/bootstrap/js/bootstrap.bundle.min.js");?>></script>
 
   <!-- Core plugin JavaScript-->
-  <script src=<?php echo base_url("assets/jquery-easing/jquery.easing.min.js"); ?>></script>
+  <script src=<?php echo base_url("assets/jquery-easing/jquery.easing.min.js");?>></script>
 
   <!-- Page level plugin JavaScript-->
-  <script src=<?php echo base_url("assets/chart.js/Chart.min.js"); ?>></script>
-  <script src=<?php echo base_url("assets/datatables/jquery.dataTables.js"); ?>></script>
-  <script src=<?php echo base_url("assets/datatables/dataTables.bootstrap4.js"); ?>></script>
+  <script src=<?php echo base_url("assets/chart.js/Chart.min.js");?>></script>
+  <script src=<?php echo base_url("assets/datatables/jquery.dataTables.js");?>></script>
+  <script src=<?php echo base_url("assets/datatables/dataTables.bootstrap4.js");?>></script>
 
   <!-- Custom scripts for all pages-->
-  <script src=<?php echo base_url("js/sb-admin.min.js"); ?>></script>
+  <script src=<?php echo base_url("js/sb-admin-2.min.js")?>></script>
 
   <!-- Demo scripts for this page-->
-  <script src=<?php echo base_url("js/demo/datatables-demo.js"); ?>></script>
-  <script src=<?php echo base_url("js/demo/chart-area-demo.js"); ?>></script>
+  <script src=<?php echo base_url("js/demo/datatables-demo.js");?>></script>
+  <script src=<?php echo base_url("js/demo/chart-area-demo.js");?>></script>
 
 </body>
 
