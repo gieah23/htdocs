5658cba885cf87ddd2297c1d7b8b90c1af878e99
Waafiq29 <45926327+Waafiq29@users.noreply.github.com>
2019-06-23 09:49:10 -0700

    update

--- projek_akhir/application/views/admin/sidebar.php
+++ projek_akhir/application/views/admin/sidebar.php
@@ -16,6 +16,11 @@
           <a class="dropdown-item" href="dataguru">Guru</a>
           <a class="dropdown-item" href="datasiswa">Siswa</a>
         </div>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">Input Data:</h6>
+          <a class="dropdown-item" href="#">Guru</a>
+          <a class="dropdown-item" href="#">Siswa</a>
+        </div>
         <li class="nav-item dropdown">
           <a class="nav-link" href="tables.html">
             <i class="fas fa-fw fa-folder"></i>
