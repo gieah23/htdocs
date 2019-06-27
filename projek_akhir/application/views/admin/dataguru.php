5b4d11bfb4e18cba650389b4e6c9d077fa221752
ambarwati20 <45646869+ambarwati20@users.noreply.github.com>
2019-06-27 17:37:37 +0700

    up

--- projek_akhir/application/views/admin/dataguru.php
+++ projek_akhir/application/views/admin/dataguru.php
@@ -1,42 +1,64 @@
-<!-- DataTables Example -->
-<div class="card mb-3">
-          <div class="card-header">
-            <i class="fas fa-table"></i>
-            Data</div>
-          <div class="card-body">
-            <div class="table-responsive">
-              <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                  <tr>
-                    <th>NIK</th>
-                    <th>Nama</th>
-                    <th>Jenis Kelamin</th>
-                    <th>Umur</th>
-                    <th>Alamat</th>
-                    <th>Mata Pelajaran</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>0987654</td>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <base href="<?=base_url()?>">
+	<meta charset="UTF-8">
+	<title>Data Guru</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <!-- Custom fonts for this template-->
+  <link href=<?php echo base_url("assets/fontawesome-free/css/all.min.css"); ?> rel="stylesheet" type="text/css">
+
+<!-- Page level plugin CSS-->
+<link href=<?php echo base_url("assets/datatables/dataTables.bootstrap4.css");?> rel="stylesheet">
+
+<!-- Custom styles for this template-->
+<link href=<?php echo base_url("css/sb-admin-2.css"); ?> rel="stylesheet">
+
+</head>
+<body>
+
+<div class="container">
+	<h1>Data Guru</h1>
+
+	<a href="data/add" class="btn btn-primary">Tambah Data Guru</a>
+
+	<table class='table table-bordered table-hover'><br><br>
+		<thead>
+			<tr>
+          <th>NIP</th>
+          <th>Nama Guru</th>
+          <th>Alamat</th>
+          <th>Email</th>
+        	<th>No HP</th>
+        	<th>Jenis Kelamin</th>
+        	<th>Kode Mapel</th>
+        	<th>Kelas</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- ISI DATA AKAN MUNCUL DISINI -->
+		        <tr>
+				  	<td>0987654</td>
                     <td>Alucard</td>
                     <td>L</td>
                     <td>11</td>
                     <td>Jalan Jalan</td>
                     <td>Bhs Inggris</td>
-                  </tr>
-                  <tr>
-                    <td>0932456</td>
-                    <td>Vexana</td>
-                    <td>P</td>
-                    <td>11</td>
-                    <td>Jalan Jalan</td>
-                    <td>Bhs Madura</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-        </div>
-      <!-- /.container-fluid -->
+					<td>30007</td>
+					<td>1</td>
+                </tr>              
+			
+		</tbody>
+	</table>
+</div>
+	
+</body>
+</html>
+
