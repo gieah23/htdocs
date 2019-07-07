48dd9332239c6d71997c4623de5f03c60c50804b
gieah23 <45646346+gieah23@users.noreply.github.com>
2019-07-07 12:07:52 +0700

    eqSasa

--- projek_akhir/application/views/admin/navbar.php
+++ projek_akhir/application/views/admin/navbar.php
@@ -2,12 +2,10 @@
 
     <a class="navbar-brand mr-1" href="index.html">SMA QWERTYUIOP</a>
 
-    <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-      <i class="fas fa-bars"></i>
-    </button>
 
+    
     <!-- Navbar -->
-    <ul class="navbar-nav ml-auto ml-md-0">
+    <ul class="navbar-nav ml-auto ml-auto mr-0 mr-md-3 my-2 my-md-0">
       <li class="nav-item dropdown no-arrow">
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i class="fas fa-user-circle fa-fw"></i>
--- projek_akhir/application/views/admin/sidebar.php
+++ projek_akhir/application/views/admin/sidebar.php
@@ -1,7 +1,7 @@
     <!-- Sidebar -->
     <ul class="sidebar navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="#">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span>
         </a>
@@ -20,18 +20,8 @@
           <h6 class="dropdown-header">Input Data:</h6>
           <a class="dropdown-item" href="#">Guru</a>
           <a class="dropdown-item" href="#">Siswa</a>
+          <a class="dropdown-item" href="#">Mata Pelajaran</a>
+          <a class="dropdown-item" href="#">Presensi</a>
         </div>
-        <li class="nav-item dropdown">
-          <a class="nav-link" href="tables.html">
-            <i class="fas fa-fw fa-folder"></i>
-              <span>Mata Pelajaran</span>
-          </a>
-        </li>        
-        <li class="nav-item dropdown">
-          <a class="nav-link" href="tables.html">
-            <i class="fas fa-fw fa-table"></i>
-              <span>Tabel Presensi</span>
-          </a>
-        </li>
       </li>
     </ul>
