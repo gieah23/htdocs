c4f23c66c9b39a1df965dee9c04357e396b8d600
Waafiq29 <45926327+Waafiq29@users.noreply.github.com>
2019-07-08 08:01:15 -0700

    p

--- projek_akhir/application/views/admin/sidebar.php
+++ projek_akhir/application/views/admin/sidebar.php
@@ -1,11 +1,7 @@
     <!-- Sidebar -->
     <ul class="sidebar navbar-nav">
       <li class="nav-item active">
-<<<<<<< HEAD
-        <a class="nav-link" href="#">
-=======
-        <a class="nav-link" href="dashboard">
->>>>>>> 82965017171a31b000005c1d623782bff017f134
+        <a class="nav-link" href="index.html">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span>
         </a>
@@ -17,20 +13,11 @@
         </a>
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
           <h6 class="dropdown-header">Data:</h6>
-          <a class="dropdown-item" href='admin/dataguru'>Guru</a>
-          <a class="dropdown-item" href='admin/datasiswa'>Siswa</a>
+          <a class="dropdown-item" href="dataguru">Guru</a>
+          <a class="dropdown-item" href="datasiswa">Siswa</a>
         </div>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-          <h6 class="dropdown-header">Input Data:</h6>
-          <a class="dropdown-item" href="#">Guru</a>
-          <a class="dropdown-item" href="#">Siswa</a>
-          <a class="dropdown-item" href="#">Mata Pelajaran</a>
-          <a class="dropdown-item" href="#">Presensi</a>
-        </div>
-<<<<<<< HEAD
-=======
         <li class="nav-item dropdown">
-        <a class="nav-link" href='admin/mapel'>
+          <a class="nav-link" href="tables.html">
             <i class="fas fa-fw fa-folder"></i>
               <span>Mata Pelajaran</span>
           </a>
@@ -41,6 +28,5 @@
               <span>Tabel Presensi</span>
           </a>
         </li>
->>>>>>> 82965017171a31b000005c1d623782bff017f134
       </li>
     </ul>
