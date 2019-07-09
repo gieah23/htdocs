058b901141f4d46d24ceae978a64d73e00569872
alvianzidny <44019028+alvianpipis@gmail.comusers.noreply.github.com>
2019-07-09 11:04:01 +0700

    Update dashboard.php

--- projek_akhir/application/views/admin/dashboard.php
+++ projek_akhir/application/views/admin/dashboard.php
@@ -42,7 +42,7 @@
           <li class="breadcrumb-item active">Overview</li>
         </ol>
         
-        <?php include 'datatable.php' ?>
+       <?php include 'isidahsboard.php' ?>
 
       </div>
       <!-- /.container-fluid -->
