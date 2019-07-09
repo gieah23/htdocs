92e0d137673d42d03b24ce842691cf6fe79872aa
alvianzidny <44019028+alvianpipis@gmail.comusers.noreply.github.com>
2019-07-09 11:23:32 +0700

    p

--- projek_akhir/application/views/admin/navbar.php
+++ projek_akhir/application/views/admin/navbar.php
@@ -1,7 +1,7 @@
 
 <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-    <a class="navbar-brand mr-1" href="index.html">ADMIN SMA MUHAMMADIYAH</a>
+    <a class="navbar-brand mr-1" href="#">ADMIN SMA MUHAMMADIYAH</a>
 
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
       <i class="fas fa-bars"></i>
